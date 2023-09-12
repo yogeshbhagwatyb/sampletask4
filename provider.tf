09013ef26385b46d2aeb7393140894eb049c33d1
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-09-12 12:45:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,6 @@
 provider "aws" {
   region  = "us-east-1" # Don't change the region
-  profile = "default"
+  #profile = "default"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "object12"
-    key    = "yogesh.bhagwat"
-    region = "us-east-1"
-  }
-}
+
